041400cbddb86733d7b2f4c0827abc01e8b17910
amandasantos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2026-08-23 10:36:42 -0300

    initial commit

---
+++ Intro/hello.dart
@@ -0,0 +1,24 @@
+void main() {
+  for(int i = 0; i < 5; i++) {
+    print('hello ${i+1}');
+  }
+
+//Variáveis:
+  var nome = 'amanda';
+  print(nome);
+
+  String texto = 'palavra';
+  print(texto);
+
+  int idade;
+  idade = 23;
+  print(idade);
+
+  double altura;
+  altura = 1.75;
+  print(altura);
+
+  bool acerto;
+  acerto = true;
+  print(acerto);  
+}
